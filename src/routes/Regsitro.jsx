3c87aae6c312a60efe08ed65d099ba03fa70fe91
Luiz Américo Valandro <meco.valandro@gmail.com>
2025-06-03 12:37:24 -0300

    Estilização tela de registros

--- src/routes/Regsitro.jsx
+++ src/routes/Regsitro.jsx
@@ -7,6 +7,12 @@ import Modal from 'react-bootstrap/Modal';
 import { FaCheck } from "react-icons/fa";
 import { LuClipboardPlus } from "react-icons/lu";
 import { TbArrowsLeftRight } from "react-icons/tb";
+import { BsBasket } from "react-icons/bs";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { HiOutlineSave } from "react-icons/hi";
+
+
+
 
 
 
@@ -65,9 +71,12 @@ function App() {
       <form onSubmit={handleSubmit} className='row g-3 justify-content-center mt-3'>
         <div className="row justify-content-center mb-3">
           <div className='col-md-6'>
-            <label htmlFor="input-data" className='form-label'>
-              Data
-            </label>
+            <div className='d-flex justify-content-center'>
+              <label htmlFor="input-data" className='form-label d-flex align-items-center gap-2'>
+                <FaRegCalendarAlt />
+                <span>Data</span>
+              </label>
+            </div>
             <input id='input-data' className='form-control' type="date" name='data' value={formData.data} onChange={handleChange} required />
           </div>
         </div>
@@ -77,49 +86,70 @@ function App() {
               <span>Situação</span>
               <TbArrowsLeftRight />
             </div>
-            <div className='row d-flex justify-content-center'>
-              <div className='d-flex justify-content-center'>
-                <label htmlFor='input-entrada' className=''>
-                  Entrada
-                </label>
-                <input id='input-entrada' type="radio" name="situacao" value="entrada" checked={formData.situacao === "entrada"} onChange={handleChange} required />
-                <label htmlFor='input-saida' className=''>
-                  Saída
-                </label>
-                <input id='input-saida' type="radio" name="situacao" value="saida" checked={formData.situacao === "saida"} onChange={handleChange} required />
+            <div className='row d-flex justify-content-center mt-3'>
+              <div className='d-flex justify-content-start gap-5'>
+                <div className='d-flex justify-content-center gap-2'>
+                  <label htmlFor='input-entrada' className=''>
+                    Entrada
+                  </label>
+                  <input className='form-check-input' id='input-entrada' type="radio" name="situacao" value="entrada" checked={formData.situacao === "entrada"} onChange={handleChange} required />
+                </div>
+
+                <div className='d-flex justify-content-start gap-2'>
+                  <label htmlFor='input-saida' className=''>
+                    Saída
+                  </label>
+                  <input className='form-check-input' id='input-saida' type="radio" name="situacao" value="saida" checked={formData.situacao === "saida"} onChange={handleChange} required />
+                </div>
               </div>
+              <p className='fs-5 fw-light text-white text-opacity-75 text-start'>Selecione a data da movimentação</p>
             </div>
           </fieldset>
-          <fieldset className='col-md-6 text-center fs-4'>
-            <legend className='fs-2'>Produto:</legend>
-            <div className='d-flex justify-content-center gap-3'>
-              <label htmlFor='input-morango'>
-                Morango
-              </label>
-              <input id='input-morango' type="radio" name="produto" value="morango" checked={formData.produto === "morango"} onChange={handleChange} required />
+          <fieldset className='col-md-6 fs-4'>
+            <div className='fs-2 fw-medium d-flex justify-content-start gap-2 align-items-center'>
+              <span>Produto</span>
+              <BsBasket />
             </div>
-            <div className='d-flex justify-content-center gap-3'>
-              <label htmlFor="input-tomate">Tomate</label>
-              <input id='input-tomate' type="radio" name="produto" value="tomate" checked={formData.produto === "tomate"} onChange={handleChange} required />
+            <div className='row d-flex justify-content-center mt-3'>
+              <div className='d-flex justify-content-start gap-5'>
+                <div className='d-flex justify-content-center gap-2'>
+                  <label htmlFor='input-morango'>
+                    Morango
+                  </label>
+                  <input className='form-check-input' id='input-morango' type="radio" name="produto" value="morango" checked={formData.produto === "morango"} onChange={handleChange} required />
+                </div>
+                <div className='d-flex justify-content-center gap-2'>
+                  <label htmlFor="input-tomate">Tomate</label>
+                  <input className='form-check-input' id='input-tomate' type="radio" name="produto" value="tomate" checked={formData.produto === "tomate"} onChange={handleChange} required />
+                </div>
+              </div>
+              <p className='fs-5 fw-light text-white text-opacity-75 text-start'>Selecione o tipo de produto</p>
             </div>
           </fieldset>
         </div>
         <div className="row">
           <div className='col-md-6'>
-            <label htmlFor="input-peso" className='form-label'>
+            <label htmlFor="input-peso" className='form-label fs-2 fw-medium'>
               Peso(Kg)
             </label>
             <input id='input-peso' className='form-control' type="number" name='peso' value={formData.peso} onChange={handleChange} required />
+            <p className='fs-5 fw-light text-white text-opacity-75 text-center'>Insira o peso em quilogramas</p>
           </div>
           <div className='col-md-6'>
-            <label htmlFor="input-valor" className='form-label'>
+            <label htmlFor="input-valor" className='form-label fs-2 fw-medium'>
               Valor(R$)
             </label>
             <input id='input-valor' className='form-control' type="number" name='valor' value={formData.valor} onChange={handleChange} required />
+            <p className='fs-5 fw-light text-white text-opacity-75 text-center'>Insira o valor em reais</p>
           </div>
         </div>
         <div className='row justify-content-center mt-5 mb-5'>
-          <button className='btn btn-rg btn-primary col-md-6' type='submit'>Registrar</button>
+          <button className='btn btn-rg btn-primary col-md-6' type='submit'>
+            <div className='fs-2 fw-medium d-flex justify-content-center gap-2 align-items-center'>
+              <HiOutlineSave />
+              <span className=''>Registrar</span>
+            </div>
+            </button>
         </div>
       </form>
 
